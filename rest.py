5ebe863539bcf0443f09cac18a8df2f2052882d3
Rishika Sushil <rishikasushil18@gmail.com>
2024-06-18 10:36:30 -0400

    Correct function for checking user geo data

--- rest.py
+++ rest.py
@@ -3,7 +3,7 @@
 import requests
 
 def isInFanCode(geo):
-    if geo['lat'] > -40 and geo['lat'] < 5 and geo['lng'] > 5 and geo['lng'] < 100:
+    if float(geo['lat']) > -40 and float(geo['lat']) < 5 and float(geo['lng']) > 5 and float(geo['lng']) < 100:
         return True
     return False
 
@@ -15,5 +15,13 @@ def percentTasksCompleted(todos):
     return (completed / len(todos)) * 100
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/users/1'
-    print(requests.get(url).json())
+    url = 'https://jsonplaceholder.typicode.com'
+    user_count = 1
+    user_data = requests.get(url + '/users/' + str(user_count)).json()
+
+    while user_data:
+        if isInFanCode(user_data['address']['geo']):
+            print("This user is in FanCode! User ID: " + str(user_count))
+        user_count += 1
+        user_data = requests.get(url + '/users/' + str(user_count)).json()
+
